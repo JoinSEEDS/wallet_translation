676f094f8de59b130867f392152f6888d3856ae7
rpiesveloces <66262181+rpiesveloces@users.noreply.github.com>
2020-06-03 08:17:59 -0600

    Update create_account.i18n.dart

--- i18n/create_account.i18n.dart
+++ i18n/create_account.i18n.dart
@@ -5,21 +5,21 @@ extension Localization on String {
 static var _t = Translations.byLocale("en_us") +
     {
       "es_es": {
-        'Please enter your name': "",
-        'Your account name should have exactly 12 symbols': "",
-        'Your account name should only contain numbers 1-5': "",
-        "Your account name can't cont'n uppercase letters": "",
-        "Your account name should cont'n lower case letters": "",
-        'Your name': "",
-        'Account Name': "",
-        'Available: ': "",
-        "Create account": "",
+        'Please enter your name': "Por favor ingresa tu nombre",
+        'Your account name should have exactly 12 symbols': "Tu nombre de cuenta debe tener exactamente 12 caracteres",
+        'Your account name should only contain numbers 1-5': "Tu nombre de cuenta puede contener números del 1 al 5",
+        "Your account name can't cont'n uppercase letters": "Tu nombre de cuenta no puede tener letras mayúsculas",
+        "Your account name should cont'n lower case letters": "Tu nombre de cuenta debe tener letras minúsculas",
+        'Your name': "Tu nombre",
+        'Account Name': "Nombre de cuenta",
+        'Available: ': "Disponible: ",
+        "Create account": "Crear cuenta",
         
         // NOTE: The sentence "your account should have exactly 12 symbols ..."
         // the words "exactly 12" are bold on the screen
-        "Your account name should have ": "",
-        "exactly 12": "",
-        " symbols (lowercase letters and digits only 1-5)": "",
+        "Tu nombre de cuenta debe tener ": "",
+        "exactamente 12": "",
+        " caracteres (letras minúsculas y números solo del 1 al 5)": "",
       }
     };
 
