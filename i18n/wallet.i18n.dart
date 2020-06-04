d2a6d5b108658a870d47be40af801c190cd0e796
rpiesveloces <66262181+rpiesveloces@users.noreply.github.com>
2020-06-04 13:19:43 -0600

    Update wallet.i18n.dart

--- i18n/wallet.i18n.dart
+++ i18n/wallet.i18n.dart
@@ -5,24 +5,24 @@ extension Localization on String {
 static var _t = Translations.byLocale("en_us") +
     {
       "es_es": {
-        'Enter user name or account': "",
-        "Transfer": "",
-        "Choose existing Seeds Member to transfer": "",
-        "Transaction hash: %s": "",
-        'Available balance': "",
-        'Transfer amount': "",
-        "Transfer amount cannot be 0.": "",
-        "Transfer amount is not valid.": "",
-        "Transfer amount cannot be greater than availabe balance.": "",
-        'Send': "",
+        'Enter user name or account': "Ingresa nombre de cuenta",
+        "Transfer": "Transferir",
+        "Choose existing Seeds Member to transfer": "Escoge un miembro de Seeds para transferir",
+        "Transaction hash: %s": "Hash de transacción: %s",
+        'Available balance': "Balance disponible",
+        'Transfer amount': "Monto a transferir",
+        "Transfer amount cannot be 0.": "Monto a transferir no puede ser 0.",
+        "Transfer amount is not valid.": "Monto a transferir no es válido",
+        "Transfer amount cannot be greater than availabe balance.": "Monto a transferir no puede ser mayor al balance disponible",
+        'Send': "Enviar",
 
-         "Enter amount": "",
-         "Confirm": "",
+         "Enter amount": "Ingresa monto",
+         "Confirm": "Confirmar",
 
-         'Your private key has not been backed up!': "",
-         'Backup': "",
-         'Later': "",
-         'Latest transactions': "",
+         'Your private key has not been backed up!': "¡Tu llave privada no ha sido respaldada en copia de seguridad!",
+         'Backup': "Copia de seguridad",
+         'Later': "Luego",
+         'Latest transactions': "Últimas transacciones",
       }
     };
 
