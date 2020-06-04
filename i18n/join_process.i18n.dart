8a2c6ab2f657b566b7c76f35a7254ba9ed343167
rpiesveloces <66262181+rpiesveloces@users.noreply.github.com>
2020-06-04 12:48:59 -0600

    Update join_process.i18n.dart

--- i18n/join_process.i18n.dart
+++ i18n/join_process.i18n.dart
@@ -5,15 +5,15 @@ extension Localization on String {
 static var _t = Translations.byLocale("en_us") +
     {
       "es_es": {
-         "Initialize new wallet...": "",
-          "Process invite link...": "",
-          "Accept invite from %s...": "",
+         "Initialize new wallet...": "Inicializar billetera nueva...",
+          "Process invite link...": "Procesar enlace de invitación...",
+          "Accept invite from %s...": "Aceptar invitación de %s...",
 
           // Example:
           // "Create account testact11111"
-          "Create account %s...": "",
-          "Import account %s...": "",
-          "Secure wallet %s...": "",
+          "Create account %s...": "Crear cuenta %s...",
+          "Import account %s...": "Importar cuenta %s...",
+          "Secure wallet %s...": "Asegurar billetera %s...",
 
       }
     };
