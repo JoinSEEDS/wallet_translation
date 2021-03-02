9b361bff2ac95c7169404173e933a5dfad87032d
baliforester <78919860+baliforester@users.noreply.github.com>
2021-03-02 17:08:17 +0800

    Update join_process.i18n.dart

--- i18n/join_process.i18n.dart
+++ i18n/join_process.i18n.dart
@@ -4,16 +4,16 @@ extension Localization on String {
 
 static var _t = Translations.byLocale("en_us") +
     {
-      "es_es": {
-         "Initialize new wallet...": "Inicializar billetera nueva...",
-          "Process invite link...": "Procesar enlace de invitación...",
-          "Accept invite from %s...": "Aceptar invitación de %s...",
+      "id_id": {
+         "Initialize new wallet...": "Inisialisasi dompet baru...",
+          "Process invite link...": "Proses tautan undangan...",
+          "Accept invite from %s...": "Terima undangan dari %s...",
 
           // Example:
           // "Create account testact11111"
-          "Create account %s...": "Crear cuenta %s...",
-          "Import account %s...": "Importar cuenta %s...",
-          "Secure wallet %s...": "Asegurar billetera %s...",
+          "Create account %s...": "Buat akun %s...",
+          "Import account %s...": "Impor akun %s...",
+          "Secure wallet %s...": "Amankan dompet %s...",
 
       }
     };
