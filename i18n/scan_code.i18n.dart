1cd8d542b3c67109be69cf327c83ce45778d9f25
baliforester <78919860+baliforester@users.noreply.github.com>
2021-03-02 11:35:14 +0800

    Update scan_code.i18n.dart

--- i18n/scan_code.i18n.dart
+++ i18n/scan_code.i18n.dart
@@ -4,8 +4,8 @@ extension Localization on String {
 
 static var _t = Translations.byLocale("en_us") +
     {
-      "es_es": {
-         "Scan failed - go back to choose another method": "Escaneo falló - regresa a escoger otro método",
+      "id_id": {
+         "Scan failed - go back to choose another method": "Pemindaian gagal - kembali untuk memilih metode lain",
       }
     };
 
