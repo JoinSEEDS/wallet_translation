1a81de1b4fa3dec1a831f180ca198a325a78b196
nik <nheger@gmail.com>
2021-03-23 12:55:47 +0800

    add google translate version

--- i18n/onboarding.i18n.dart
+++ i18n/onboarding.i18n.dart
@@ -5,9 +5,6 @@ extension Localization on String {
 static var _t = Translations.byLocale("en_us") +
     {
       "id_id": {
-        
-        // TRANSLATION NOTE: These are automatic translations by Google Translate - please replace
-
         'Better than free transactions': 'Lebih baik daripada transaksi gratis',
         "Make payments globally without any fees. Earn rewards when you support 'regenerative' organisations and people.":
         "Lakukan pembayaran secara global tanpa biaya apa pun. Dapatkan imbalan jika Anda mendukung organisasi dan orang 'regeneratif''.",
